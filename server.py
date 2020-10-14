6b79c5165e57738d33cfa6476d7a01c3db5d782b
Azri <59246759+katnaps@users.noreply.github.com>
2020-10-14 16:00:18 +0800

    show name from url parameter in template

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+@app.route("/<name>")
+def index(name):
+    name = name.upper()
+    return render_template('index.html', name=name)
 
-@app.route("/extra")
-def extra():
-    return '<h1>Extra Page<h2>'
+# @app.route("/extra")
+# def extra():
+#     return '<h1>Extra Page<h2>'
 
-@app.route("/user/<username>")
-def user(username):
-    return f"Hi {username}"
+# @app.route("/user/<username>")
+# def user(username):
+#     return f"Hi {username}"
 
 
 if __name__ == '__main__':
