e1385af5561f1fd88da35043d0d91132de592e33
Azri <59246759+katnaps@users.noreply.github.com>
2020-10-14 15:17:40 +0800

    added templates folders with html file

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<h1>Hello There...</h1>'
+    return render_template('index.html')
 
 @app.route("/extra")
 def extra():
