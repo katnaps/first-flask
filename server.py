80eafc885302be760a1355f7c15ddcbc66510d2e
Azri <59246759+katnaps@users.noreply.github.com>
2020-10-14 15:04:37 +0800

    Added extra route

--- server.py
+++ server.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello There...</h1>'
 
+@app.route("/extra")
+def extra():
+    return '<h1>Extra Page<h2>'
+
 
 if __name__ == '__main__':
     app.run()
